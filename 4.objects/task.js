768194000c8f2da7de36a5c7d8b24f163842ccde
Dmitriy K <kiryuschev2012@gmail.com>
2023-10-24 21:19:43 +0300

    Четвёртое задание, исправления

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,9 +5,9 @@ function Student(name, gender, age) {
   this.marks = [];
 }
 
-const student1 = new Student('John', 'male', 21);
-const student2 = new Student('Jane', 'female', 20);
-const student3 = new Student('Michael', 'male', 19);
+const student1 = new Student('Женя', 'мужской', 21);
+const student2 = new Student('Жанна', 'женский', 20);
+const student3 = new Student('Михаил', 'мужской', 19);
 console.log(student1);
 console.log(student2);
 console.log(student3);
@@ -17,10 +17,13 @@ Student.prototype.setSubject = function(subjectName) {
 }
 
 Student.prototype.addMarks = function(...marksToAdd) {
-  if (!this.marks) {
- 	throw new Error('евозможно добавить оценки, т.к.пользователь не является студентом');
+  if (this.hasOwnProperty('marks')) {
+    for (let mark of marksToAdd) {
+    this.marks.push(mark);
+    }
+  } else {
+  return;
   }
-  this.marks.push(...marksToAdd);
 }
 const student4 = new Student('Bob', 'male', 22);
 student4.addMarks(100, 90, 80, 70, 60);
@@ -34,8 +37,8 @@ Student.prototype.getAverage = function() {
 }
 
 Student.prototype.exclude = function(reason) {
-  this.subject = undefined;
-  this.marks = undefined;
+  delete subject;
+  delete marks;
     
-  this.excluded = reason;
+  this.excluded = reason('Невозможно добавить оценки, т.к.пользователь не является студентом');
 }
